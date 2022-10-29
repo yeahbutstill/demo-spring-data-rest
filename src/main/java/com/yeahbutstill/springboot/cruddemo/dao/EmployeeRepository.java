0b3361c7a7772338e5043266840f0041343285e5
yeahbutstill <mayadani28@gmail.com>
2022-10-29 15:19:56 +0700

    update

--- src/main/java/com/yeahbutstill/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/yeahbutstill/springboot/cruddemo/dao/EmployeeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	// that's it ... no need to write any code LOL!
+    // that's it ... no need to write any code LOL!
 
 }
 
--- src/main/java/com/yeahbutstill/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/yeahbutstill/springboot/cruddemo/entity/Employee.java
@@ -3,79 +3,79 @@ package com.yeahbutstill.springboot.cruddemo.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
 
-	// define fields
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	private int id;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-	private String lastName;
-	
-	@Column(name="email")
-	private String email;
-	
-		
-	// define constructors
-	
-	public Employee() {
-		
-	}
+    // define fields
 
-	public Employee(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
-	// define getter/setter
-	
-	public int getId() {
-		return id;
-	}
+    @Column(name = "first_name")
+    private String firstName;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    @Column(name = "last_name")
+    private String lastName;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    @Column(name = "email")
+    private String email;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
 
-	public String getLastName() {
-		return lastName;
-	}
+    // define constructors
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public Employee() {
 
-	public String getEmail() {
-		return email;
-	}
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-	// define tostring
+    // define getter/setter
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-	}
-		
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // define tostring
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+
 }
 
 
